b7dc77e3580148912c826949c5ecb1dd1b77331a
Adil <altynbekadil1129@gmail.com>
2026-01-18 23:09:48 +0500

    added in url and password

--- src/config/DatabaseConfig.java
+++ src/config/DatabaseConfig.java
@@ -5,14 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    private static final String URL = "";
+    private static final String URL = "jdbc:postgresql://localhost:5432/cinemadb";
     private static final String USER = "";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "asd222555";
 
     private static Connection connection = null;
 
     public static Connection  getConnection() throws SQLException {
-        if (connection == null connection.isClosed()); {
+        if (connection == null || connection.isClosed()); {
 
         }
     }
