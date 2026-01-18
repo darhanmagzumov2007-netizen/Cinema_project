4aaee3362039477d2a5df4ed36b12a23171bb650
Adil <altynbekadil1129@gmail.com>
2026-01-18 22:52:00 +0500

    added some in database config

--- src/config/DatabaseConfig.java
+++ src/config/DatabaseConfig.java
@@ -3,3 +3,21 @@ package config;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+public class DatabaseConfig {
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
+
+    private static Connection connection = null;
+
+    public static Connection  getConnection() throws SQLException {
+        if (connection == null connection.isClosed()); {
+
+        }
+    }
+
+
+
+
+}
