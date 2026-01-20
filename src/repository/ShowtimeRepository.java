1fbf6858a6430ce536c4e1bf82b6df4cbaa58242
Adil <altynbekadil1129@gmail.com>
2026-01-20 22:07:22 +0500

    define CRUD methods for ShowtimeRepository

--- src/repository/ShowtimeRepository.java
+++ src/repository/ShowtimeRepository.java
@@ -1,4 +1,12 @@
 package repository;
 
-public class ShowtimeRepository {
+import entity.Showtime;
+import java.sql.SQLException;
+
+public interface ShowtimeRepository {
+    Showtime save(Showtime showtime) throws SQLException;
+    Showtime findById(Integer id) throws SQLException;
+    List<Showtime> findAll() throws SQLException;
+    void update(Showtime showtime) throws SQLException;
+    void delete(Integer Id) throws SQLException;
 }
