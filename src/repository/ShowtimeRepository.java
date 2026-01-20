2ca14b0ee7456f2129b1854d4a94974bf8e84a4f
Adil <altynbekadil1129@gmail.com>
2026-01-20 22:50:27 +0500

    add business-specific query methods to ShowtimeRepository

--- src/repository/ShowtimeRepository.java
+++ src/repository/ShowtimeRepository.java
@@ -2,11 +2,15 @@ package repository;
 
 import entity.Showtime;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ShowtimeRepository {
     Showtime save(Showtime showtime) throws SQLException;
     Showtime findById(Integer id) throws SQLException;
     List<Showtime> findAll() throws SQLException;
+    List<Showtime> findByMovieId(Integer movieId) throws SQLException;
+    List<Showtime> findByDate(LocalDate date) throws SQLException;
     void update(Showtime showtime) throws SQLException;
     void delete(Integer Id) throws SQLException;
 }
