4703f460e52654eff74ab4c0af795289da330c8f
Nurali2508 <250291@astanait.edu.kz>
2026-01-20 22:26:21 +0500

    Update Movie.java

--- src/entity/Movie.java
+++ src/entity/Movie.java
@@ -5,88 +5,79 @@ import java.time.LocalDate;
 public class Movie {
     private Integer id;
     private String title;
-    private Integer duration;
     private String genre;
+    private Integer duration; // in minutes
     private Double rating;
-    private LocalDate releaseData;
-
-
-    public Movie() {
-    }
-
-
-
-    public Integer getDuration() {
-        return duration;
-    }
-
-    public void setDuration(Integer duration) {
+    private LocalDate releaseDate;
+    
+    public Movie() {}
+    
+    public Movie(Integer id, String title, String genre, Integer duration, Double rating, LocalDate releaseDate) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
         this.duration = duration;
+        this.rating = rating;
+        this.releaseDate = releaseDate;
     }
 
     public Integer getId() {
         return id;
     }
-
+    
     public void setId(Integer id) {
         this.id = id;
     }
-
+    
     public String getTitle() {
         return title;
     }
-
+    
     public void setTitle(String title) {
         this.title = title;
     }
-
+    
     public String getGenre() {
         return genre;
     }
-
+    
     public void setGenre(String genre) {
         this.genre = genre;
     }
-
+    
+    public Integer getDuration() {
+        return duration;
+    }
+    
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+    
     public Double getRating() {
         return rating;
     }
-
+    
     public void setRating(Double rating) {
         this.rating = rating;
     }
-
-    public LocalDate getReleaseData() {
-        return releaseData;
+    
+    public LocalDate getReleaseDate() {
+        return releaseDate;
     }
-
-    public void setReleaseData(LocalDate releaseData) {
-        this.releaseData = releaseData;
-    }
-
-     public Movie(Integer id, String title, String genre, Integer duration, Double rating, LocalDate releaseData) {
-
-
-        this.id = id;
-        this.title = title;
-        this.duration = duration;
-        this.genre = genre;
-        this.rating = rating;
-        this.releaseData = releaseData;
-
+    
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
     }
 
     @Override
     public String toString() {
         return "Movie{" +
                 "id=" + id +
-                ", title=" + title + '\'' +
+                ", title='" + title + '\'' +
                 ", genre='" + genre + '\'' +
                 ", duration=" + duration +
                 ", rating=" + rating +
-                ", releaseDate=" + releaseData +
+                ", releaseDate=" + releaseDate +
                 '}';
     }
-
-
 }
