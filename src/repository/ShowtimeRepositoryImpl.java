dc1bb9e6cf1d28e4bd5b4edd53ecd76e42434a79
Adil <altynbekadil1129@gmail.com>
2026-01-21 13:40:11 +0500

    implement save method for Showtime

--- src/repository/ShowtimeRepositoryImpl.java
+++ src/repository/ShowtimeRepositoryImpl.java
@@ -1,4 +1,35 @@
 package repository;
 
-public class ShowtimeRepositoryImpl {
+import config.DatabaseConfig;
+import entity.Showtime;
+import java.sql.*;
+
+
+public class ShowtimeRepositoryImpl implements ShowtimeRepository {
+
+    @Override
+    public Showtime save(Showtime showtime) throws SQLException {
+        String sql = "INSERT INTO showtimes (movie_id, hall_id, show_date, show_time, price) VALUES (?, ?, ?, ?, ?) RETURNING id";
+
+        try (Connection conn = DatabaseConfig.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, showtime.getMovieId());
+            stmt.setInt(2, showtime.getHallId());
+            stmt.setDate(3, Date.valueOf(showtime.getShowDate()));
+            stmt.setTime(4, Time.valueOf(showtime.getShowTime()));
+            stmt.setDouble(5, showtime.getPrice());
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                showtime.setId(rs.getInt("id"));
+            }
+
+            return showtime;
+
+            }
+        }
+
+    }
 }
