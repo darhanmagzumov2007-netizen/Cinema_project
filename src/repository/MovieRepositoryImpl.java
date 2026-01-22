2c753ab3e6e188ade241d7786544746cad3d4489
Adil <altynbekadil1129@gmail.com>
2026-01-22 16:39:14 +0500

    implement save and findById methods

--- src/repository/MovieRepositoryImpl.java
+++ src/repository/MovieRepositoryImpl.java
@@ -3,64 +3,62 @@ package repository;
 import config.DatabaseConfig;
 import entity.Movie;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class MovieRepositoryImpl implements MovieRepository {
-}
-@Override
-public Movie save(Movie movie) throws SQLException {
-    String sql = "INSERT INTO movies (title, genre, duration, rating, release date) VALUES(?, ?. ?. ?, ?) RETURNING id";
-    try (Connection conn = DatabaseConfig.getConnection();
-         PreparedStatement stmt = conn.prepareStatement(sql)) {
-        stmt.setString(1, movie.getTitle());
-        stmt.setString(2, movie.getGenre());
-        stmt.setInt(3, movie.getDuration());
-        stmt.setDouble(4, movie.getRating());
 
-        ResultSet rs = stmt.executeQuery();
+    @Override
+    public Movie save(Movie movie) throws SQLException {
+        String sql = "INSERT INTO movies (title, genre, duration, rating, release_date) VALUES (?, ?, ?, ?, ?) RETURNING id";
 
+        try (Connection conn = DatabaseConfig.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-        if (rs.next()) {
-            movie.setId(rs.getInt("id"));
+            stmt.setString(1, movie.getTitle());
+            stmt.setString(2, movie.getGenre());
+            stmt.setInt(3, movie.getDuration());
+            stmt.setDouble(4, movie.getRating());
+            stmt.setDate(5, Date.valueOf(movie.getReleaseDate()));
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                movie.setId(rs.getInt("id"));
+            }
+
+            return movie;
         }
-        return movie;
     }
-}
 
-@Override
-public Movie findById(Integer id) throws SQLException {
+    public Movie findById(Integer id) throws SQLException {
+        String sql = "SELECT * FROM movies WHERE id = ?";
 
-    String sql = "SELECT * FROM movies WHERE id = ?";
-    try (Connection conn = DatabaseConfig.getConnection();
-    (PreparedStatement stmt = conn.prepareStatement(sql)){
-        stmt.setInt(1, id);
-        ResultSet rs = stmt.executeQuery();
-        if (rs.next()){
-            return (rs);
+        try (Connection conn = DatabaseConfig.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                Movie movie = new Movie();
+                movie.setId(rs.getInt("id"));
+                movie.setTitle(rs.getString("title"));
+                movie.setGenre(rs.getString("genre"));
+                movie.setDuration(rs.getInt("duration"));
+                movie.setRating(rs.getDouble("rating"));
+                movie.setReleaseDate(rs.getDate("release_date").toLocalDate());
+                return movie;
+            }
         }
+
         return null;
-
-
-        @Override
-        public List<Movie> findAll() throws SQLException {
-
-            String sql = "SELECT * FROM movies ORDER BY id";
-           List<Movie> movies = new ArrayList<>();
-           try (Connection conn = DatabaseConfig.getConnection();
-           Statement stmt = conn.createStatement();
-           ResultSet rs = stmt.executeQuery(sql)){
-
-               while (rs.next()){
-                   movies.add((rs));
-               }
-
-           }
-           }
     }
-
-
-
-
-
 }
 
