f32470bc03e1c3eb3f5838d6e12bad48d09db2ba
darhanmagzumov2007-netizen <darhanmagzumov2007@gmail.com>
2026-01-22 18:10:17 +0500

    MovieRepository

--- src/repository/MovieRepository.java
+++ src/repository/MovieRepository.java
@@ -1,4 +1,16 @@
 package repository;
 
-public class MovieRepository {
+
+import entity.Movie;
+import java.sql.SQLException;
+import java.util.List;
+
+
+public interface MovieRepository {
+    Movie save(Movie movie) throws SQLException;
+    Movie findById(Integer id) throws SQLException;
+    List<Movie> findAll() throws SQLException;
+    List<Movie> findByGenre(String genre) throws SQLException;
+    void update (Movie movie) throws SQLException;
+    void delete(Integer id) throws SQLException;
 }
