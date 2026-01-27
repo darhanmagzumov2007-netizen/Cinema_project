8e41743387be79ac5829e56f46bd75e37a3bf345
Adil <altynbekadil1129@gmail.com>
2026-01-27 22:48:48 +0500

    add full constructor to FullShowtimeDTO

--- src/dto/FullShowTimeDTO.java
+++ src/dto/FullShowTimeDTO.java
@@ -1,4 +1,41 @@
 package dto;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class FullShowTimeDTO {
+    private Integer showtimeId;
+    private LocalDate showDate;
+    private LocalTime showTime;
+    private Double price;
+
+    private Integer movieId;
+    private String movieTitle;
+    private String movieGenre;
+    private String movieCategory;
+    private Integer movieDuration;
+    private Double movieRating;
+
+    private Integer hallId;
+    private String hallName;
+    private Integer hallCapacity;
+
+    public FullShowtimeDTO() {}
+
+    public FullShowtimeDTO(Integer showtimeId, LocalDate showDate, LocalTime showTime, Double price, Integer movieId, String movieTitle, String movieGenre, String movieCategory, Integer movieDuration, Double movieRating, Integer hallId, String hallName, Integer hallCapacity) {
+        this.showtimeId = showtimeId;
+        this.showDate = showDate;
+        this.showTime = showTime;
+        this.price = price;
+        this.movieId = movieId;
+        this.movieTitle = movieTitle;
+        this.movieGenre = movieGenre;
+        this.movieCategory = movieCategory;
+        this.movieDuration = movieDuration;
+        this.movieRating = movieRating;
+        this.hallId = hallId;
+        this.hallName = hallName;
+        this.hallCapacity = hallCapacity;
+    }
+
 }
